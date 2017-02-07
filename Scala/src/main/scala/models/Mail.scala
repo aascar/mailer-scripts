190f4d09b398cd2c8a6119744284e3a4695654ae
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-02-08 00:55:34 +0530

    Mail model

--- Scala/src/main/scala/models/Mail.scala
+++ Scala/src/main/scala/models/Mail.scala
@@ -3,6 +3,13 @@ package models
 /**
   * Created by jyothi on 7/2/17.
   */
-class Mail {
-
-}
+case class Mail(
+                 from: (String, String), // (email -> name)
+                 to: Seq[String],
+                 cc: Seq[String] = Seq.empty,
+                 bcc: Seq[String] = Seq.empty,
+                 subject: String,
+                 message: String,
+                 richMessage: Option[String] = None,
+                 attachment: Option[(java.io.File)] = None
+               )
